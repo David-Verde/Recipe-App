b06f8428240fe26fb9af1aeeb9877b89eb4a0405
Unyielding1 <david.verde.alvarez@gmail.com>
2022-10-19 14:25:26 -0500

    add code to index

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_221048) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_163505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.string "quantity"
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
