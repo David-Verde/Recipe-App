aa93469bcf2f036ac962160af0585cb738e7210a
Unyielding1 <david.verde.alvarez@gmail.com>
2022-10-19 15:13:13 -0500

    fix stylelint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,14 @@
 class ApplicationController < ActionController::Base
-
   def current_user
     @current_user = User.first
   end
 end
 
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+before_action :authenticate_user!
+before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
-  end
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
 end
