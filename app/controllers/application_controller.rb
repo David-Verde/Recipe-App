90cf277bbf375db262a0cf63bd0ea68f581418e0
Unyielding1 <david.verde.alvarez@gmail.com>
2022-10-20 10:24:27 -0500

    Create navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user = User.first
-  end
-end
+protect_from_forgery with: :exception
 
 before_action :authenticate_user!
 before_action :configure_permitted_parameters, if: :devise_controller?
@@ -11,4 +8,5 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
 def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
+end
 end
